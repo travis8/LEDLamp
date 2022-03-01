d10c3159e67caa5f1e341b645c7b9455445f4213
Travis W <twentz@avanleecare.com>
2022-02-28 17:28:29 -0700

    scaling current based on number of leds

--- LEDLamp.ino
+++ LEDLamp.ino
@@ -1,19 +1,20 @@
 #include <FastLED.h>
 #define DATA_PIN 2
-#define NUM_LEDS 16
+#define NUM_LEDS 32
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setMaxPowerInVoltsAndMilliamps(5,500);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5,35 * NUM_LEDS);
   FastLED.clear();
   FastLED.show();
+  FastLED.setBrightness(255);
 }
 
 void loop() {
-  leds[0] = CRGB(255, 0, 0);
-  leds[1] = CRGB(0, 255, 0);
-  leds[2] = CRGB(0, 0, 255);
-//  FastLED.show();
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(255, 255, 255);
+  }
+  FastLED.show();
 }
